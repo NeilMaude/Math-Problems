e8782b617f7e717ff4b31aa4e197fd80e40c7e73
Neil Maude <neil.maude@gmail.com>
2018-12-26 18:02:47 +0000

    Correct solution

---
+++ Problem1/problem1.py
@@ -0,0 +1,34 @@
+# Project Euler - problem #1
+# Count values which are a multiple of 3 or 5 in natural numbers up to N
+
+import sys
+
+def isMultiple(n):
+    if n % 3 == 0:
+        #print('%s mod 3 is zero' % n)
+        return True
+    elif n % 5 == 0:
+        #print('%s mod 5 is zero' % n)
+        return True
+    else:
+        return False
+
+def main(N):
+    print('Main calculation')
+    count = 0
+    sum = 0
+    for i in range(1,N):
+        if isMultiple(i):
+            count += 1
+            sum += i
+    return count, sum
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print('Running unit tests...')
+        print('Hint: there are no unit tests...')
+    else:
+        print('Running main calculation')
+        print('Counting multiples up to : %s' % sys.argv[1])
+        count, sum = main(int(sys.argv[1]))
+        print('Result for %s is %s, with count of %s' % (sys.argv[1], sum, count))
